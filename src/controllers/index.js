db5f985a0ce2faaad4d0352a6269d66753ba9b11
Alex Chirkin <ihoooly@gmail.com>
2017-06-02 16:24:37 +0300

    some changes

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -3,6 +3,6 @@ module.exports = {
 	default: function( req, res ) {
 
 		const params = Object.assign( { title: 'Ковровское карьеоуправление', bodyClass: 'page-index' }, req.params )
-		res.render('indexNew', params)
+		res.render('index', params)
 	}
 }
